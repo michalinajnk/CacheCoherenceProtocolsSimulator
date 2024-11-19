efbe33ba9d8de55970ae453b5ca15e06bf7ecceb
michalinajnk <254564@student.pwr.edu.pl>
2024-11-19 17:46:51 +0800

    Fixing bugs MESI

--- Simulation_Scripts/simulation_test_powershell.ps1
+++ Simulation_Scripts/simulation_test_powershell.ps1
@@ -1,14 +1,14 @@
 Write-Output "===blackscholes==="
 
-python .\coherence.py MESI blackscholes 1024 2 16 4
-python .\coherence.py Dragon blackscholes 1024 2 16 4
+python C:\Users\Michalina\cs4223\Assignment2\CacheCoherenceProtocolsSimulator\coherence.py MESI blackscholes 1024 2 16 4
+python C:\Users\Michalina\cs4223\Assignment2\CacheCoherenceProtocolsSimulator\coherence.py Dragon blackscholes 1024 2 16 4
 
 Write-Output "===fluidanimate==="
 
-python .\coherence.py MESI fluidanimate 1024 2 16 4
-python .\coherence.py Dragon fluidanimate 1024 2 16 4
+python C:\Users\Michalina\cs4223\Assignment2\CacheCoherenceProtocolsSimulator\coherence.py.py MESI fluidanimate 1024 2 16 4
+python C:\Users\Michalina\cs4223\Assignment2\CacheCoherenceProtocolsSimulator\coherence.py.py Dragon fluidanimate 1024 2 16 4
 
 Write-Output "===bodytrack==="
 
-python .\coherence.py MESI bodytrack 1024 1 16 4
-python .\coherence.py Dragon bodytrack 1024 1 16 4
+python C:\Users\Michalina\cs4223\Assignment2\CacheCoherenceProtocolsSimulator\coherence.py MESI bodytrack 1024 2 16 4
+python C:\Users\Michalina\cs4223\Assignment2\CacheCoherenceProtocolsSimulator\coherence.py Dragon bodytrack 1024 2 16 4
